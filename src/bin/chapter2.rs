00293daf902ae43a7e6ba9c0159a0e8e4a6588be
mubasharameen485-cloud <mubasharameen485@gmail.com>
2026-07-19 09:33:05 +0500

    chap2

--- src/bin/chapter2.rs
+++ src/bin/chapter2.rs
@@ -1,5 +1,8 @@
 fn main() {
-let tup = (500, 6.4, 1);
-let (x, y, z) = tup;
-println!("The value of y is: {}", y);
+    let a = [10, 20, 30, 40, 50];
+
+    // For loop: 'a' mein jitne bhi elements hain, bari bari unhe 'element' mein dalo
+    for element in a {
+        println!("the value is: {}", element);
+    }
 }
