89caeb4b74cb47ee7fb4de678b534e77a16644ab
mubasharameen485-cloud <mubasharameen485@gmail.com>
2026-07-21 09:32:03 +0500

    finalchapter2

--- src/bin/chapter2finalproject.rs
+++ src/bin/chapter2finalproject.rs
@@ -1,35 +1,42 @@
-const FREEZING_POINT_F:f64=32.0;
-fn analyze_tem(celsius:f64)->(f64,char){
-    cal=((celsius * 1.8) + 32.0);
-if cal > FREEZING_POINT_F{
-    print!("F");
+const FREEZING_POINT_F: f64 = 32.0;
+
+fn analyze_tem(celsius: f64) -> (f64, char) {
+    let cal = (celsius * 1.8) + 32.0;
+
+    
+    let status = if cal <= FREEZING_POINT_F {
+        'F'
+    } else if cal > 80.0 {
+        'H'
+    } else {
+        'N'
+    };
+
+    
+    (cal, status)
 }
-else if  cal<80.0 {
-    print!("H")
-}else {
-    print!("N")
-};
-(f,char)
 
-}
-
-
-fn main(){
-    let temps_celsius:  =[0.0, 40.0, 25.0, -10.0, 15.0];
-    'system_loop:loop{
-        print!("Analyzing weather data...");
-        for temp in temps_celsius{
-           analyze_temp(temp);
-           let (f_val, status) = analyze_temp(temp);
-
-
-    if status=='F'{
-        print!("X degrees F is Freezing! ❄️");
-        }else if status == 'H {
-            print!("X degrees F is Hot! 🔥");
-        }else if status == 'N{
-            print!("X degrees F is Normal Weather. ⛅");
+fn main() {
+    let temps_celsius: [f64; 5] = [0.0, 40.0, 25.0, -10.0, 15.0];
+    
+    'system_loop: loop {
+        println!("Analyzing weather data...\n"); 
         
+        for temp in temps_celsius {
+           
+           let (f_val, status) = analyze_tem(temp);
+
+           
+           if status == 'F' {
+               println!("{} degrees F is Freezing! ", f_val);
+           } else if status == 'H' {
+               println!("{} degrees F is Hot! ", f_val);
+           } else if status == 'N' {
+               println!("{} degrees F is Normal Weather. ", f_val);
+           }
         }
+        
+        
+        break 'system_loop;
     }
 }
