57077702e9e67fdf633a380ea78312eb343b70d2
mubasharameen485-cloud <mubasharameen485@gmail.com>
2026-08-31 09:56:23 +0500

    mod practice

---
+++ src/bin/mode.rs
@@ -0,0 +1,14 @@
+
+mod sound {
+    
+    pub mod instrument {
+        pub fn clarinet() {
+            println!("Clarinet okkkk!");
+        }
+    }
+}
+
+fn main() {
+    
+    sound::instrument::clarinet();
+}
